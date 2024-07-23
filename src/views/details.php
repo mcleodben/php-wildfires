1af0bd81a0d339f5db22f3d3458030150c91964f
Ben Mcleod <ben-mcleod@live.co.uk>
2024-07-23 21:00:22 +0100

    Add some styling to the tables

--- src/views/details.php
+++ src/views/details.php
@@ -1,30 +1,32 @@
 <?php include_once __DIR__ . '/layouts/header.php'; ?>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Fire Name</th>
-            <th>Year</th>
-            <th>Cause</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php if (!empty($data['fires'])): ?>
-            <?php foreach ($data['fires'] as $row): ?>
+<div>
+    <table class="shadow-2xl border-2 border-orange-900">
+        <thead class="bg-orange-400">
             <tr>
-                <th><?= $row['FPA_ID']; ?></th>
-                <th><?= $row['FIRE_NAME']; ?></th>
-                <th><?= $row['FIRE_YEAR']; ?></th>
-                <th><?= $row['STAT_CAUSE_DESCR']; ?></th>
+                <th class="py-3">ID</th>
+                <th class="py-3">Fire Name</th>
+                <th class="py-3">Year</th>
+                <th class="py-3">Cause</th>
             </tr>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <tr>
-                <td>No details for this fire</td>
-            </tr>
-        <?php endif; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php if (!empty($data['fires'])): ?>
+                <?php foreach ($data['fires'] as $row): ?>
+                <tr class="bg-orange-300 hover:bg-orange-100 duration-300">
+                    <th class="py-3 px-6"><?= $row['FPA_ID']; ?></th>
+                    <th class="py-3 px-6"><?= $row['FIRE_NAME']; ?></th>
+                    <th class="py-3 px-6"><?= $row['FIRE_YEAR']; ?></th>
+                    <th class="py-3 px-6"><?= $row['STAT_CAUSE_DESCR']; ?></th>
+                </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td>No details for this fire</td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+</div>
 
 <?php include_once __DIR__ . '/layouts/footer.php' ?>
--- src/views/home.php
+++ src/views/home.php
@@ -6,18 +6,18 @@
     </div>
     <hr>
 
-    <table>
-        <thead>
+    <table class="shadow-2xl border-2 border-orange-900 opacity-75">
+        <thead class="bg-orange-400">
             <tr>
-                <th>Forest Name.</th>
+                <th class="py-3">Forest Name</th>
             </tr>
         </thead>
 
-        <tbody>
+        <tbody class="bg-orange-300">
             <?php if (!empty($data['forests'])): ?>
                 <?php foreach ($data['forests'] as $forest): ?>
-                    <tr>
-                        <th>
+                    <tr class="hover:bg-orange-100 duration-300">
+                        <th lass="py-3 px-6">
                             <a href="/details?forest=<?= $forest['NWCG_REPORTING_UNIT_NAME'] ?>">
                                 <?= $forest['NWCG_REPORTING_UNIT_NAME']; ?>
                             </a>
