90c08f53b63a337461a88fb26f5fe1681f1d4e1b
Ben Mcleod <ben-mcleod@live.co.uk>
2024-07-23 20:15:58 +0100

    Test connection to db

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-    echo 'Hello wildfires';
+    $dbConnection = new PDO('sqlite:FPA_FOD_20170508.sqlite');
+
+    $forests = $dbConnection->query("SELECT DISTINCT NWCG_REPORTING_UNIT_NAME FROM Fires")->fetchAll(PDO::FETCH_ASSOC);
+
+    var_dump($forests);
 ?>
